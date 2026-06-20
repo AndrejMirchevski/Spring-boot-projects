e565e2f8e9dcbcebeff819b08be286631c8b5d10
AndrejMirchevski <amircevski2005@gmail.com>
2026-06-20 15:19:00 +0200

    test

---
+++ Real-Estate-Listings-API/Real-Estate-Listings-API/src/main/java/com/example/Real_Estate_Listings_API/RealEstateListingsApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.Real_Estate_Listings_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealEstateListingsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealEstateListingsApiApplication.class, args);
+	}
+
+}
---
+++ Real-Estate-Listings-API/Real-Estate-Listings-API/src/test/java/com/example/Real_Estate_Listings_API/RealEstateListingsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Real_Estate_Listings_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealEstateListingsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
