88bacb97d4955303abab8101a23ac5ec3bb6c584
AndrejMirchevski <amircevski2005@gmail.com>
2026-06-21 13:57:33 +0200

    init commit

---
+++ SocialMediaCommentingAPI/SocialMediaCommentingAPI/src/main/java/com/example/SocialMediaCommentingAPI/SocialMediaCommentingApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.SocialMediaCommentingAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialMediaCommentingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialMediaCommentingApiApplication.class, args);
+	}
+
+}
---
+++ SocialMediaCommentingAPI/SocialMediaCommentingAPI/src/test/java/com/example/SocialMediaCommentingAPI/SocialMediaCommentingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SocialMediaCommentingAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaCommentingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
