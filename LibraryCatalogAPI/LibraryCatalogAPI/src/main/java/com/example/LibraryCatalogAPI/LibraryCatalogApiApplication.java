cac18196a74f5fb62f175b3e096be10d5c08787c
AndrejMirchevski <amircevski2005@gmail.com>
2026-06-24 01:40:14 +0200

    entity, dto, repository, exception

---
+++ LibraryCatalogAPI/LibraryCatalogAPI/src/main/java/com/example/LibraryCatalogAPI/LibraryCatalogApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.LibraryCatalogAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryCatalogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryCatalogApiApplication.class, args);
+	}
+
+}
