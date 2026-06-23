f97ca36e4d46e47127d1384b8b3c2b4875e1b460
AndrejMirchevski <amircevski2005@gmail.com>
2026-06-23 16:51:05 +0200

    init and some entites

---
+++ RecipeBookAPI/RecipeBookAPI/src/main/java/com/example/RecipeBookAPI/RecipeBookApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.RecipeBookAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeBookApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeBookApiApplication.class, args);
+	}
+
+}
---
+++ RecipeBookAPI/RecipeBookAPI/src/test/java/com/example/RecipeBookAPI/RecipeBookApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RecipeBookAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeBookApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
