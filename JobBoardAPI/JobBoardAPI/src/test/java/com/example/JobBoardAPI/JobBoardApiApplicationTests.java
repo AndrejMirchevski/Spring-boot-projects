f7b5204f5c9742625f845bd284d475df8a6edc8e
AndrejMirchevski <amircevski2005@gmail.com>
2026-06-25 19:01:38 +0200

    init

---
+++ JobBoardAPI/JobBoardAPI/src/test/java/com/example/JobBoardAPI/JobBoardApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JobBoardAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobBoardApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
