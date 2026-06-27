29050e947cc88af67913e207ef4ac0ae4002edef
AndrejMirchevski <amircevski2005@gmail.com>
2026-06-27 02:16:24 +0200

    not working

---
+++ MovieReviewsAPI/src/main/java/com/example/MovieReviewsAPI/MovieReviewsApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.MovieReviewsAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieReviewsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieReviewsApiApplication.class, args);
+	}
+
+}
---
+++ MovieReviewsAPI/src/test/java/com/example/MovieReviewsAPI/MovieReviewsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MovieReviewsAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieReviewsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
